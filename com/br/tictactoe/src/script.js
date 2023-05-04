0e1a078386fbcefdf5acff1e2ca907761fd4e910
Gabriel <prismaticnetwork12@gmail.com>
2023-05-03 21:29:28 -0300

    feat: implementing minimax algorithm

--- com/br/tictactoe/src/script.js
+++ com/br/tictactoe/src/script.js
@@ -17,6 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+function getBoard() {
+  const cells = document.querySelectorAll(".cell");
+  const board = [[], [], []];
+  cells.forEach((cell, index) => {
+    const rowIndex = Math.floor(index / 3);
+    const columnIndex = index % 3;
+    board[rowIndex][columnIndex] = cell.textContent;
+  });
+  return board;
+}
+
 function handleCellClick(event) {
   const clickedCell = event.target;
 
@@ -34,31 +45,121 @@ function handleCellClick(event) {
 }
 
 function computerPlay() {
+  const bestMove = findBestMove(getBoard());
   const cells = document.querySelectorAll(".cell");
-  const emptyCells = Array.from(cells).filter(
-    (cell) => cell.textContent === ""
-  );
+  const chosenCell = cells[bestMove.row * 3 + bestMove.col];
+  chosenCell.textContent = "O";
 
-  if (emptyCells.length > 0) {
-    const randomIndex = Math.floor(Math.random() * emptyCells.length);
-    const randomEmptyCell = emptyCells[randomIndex];
+  if (gameOver()) {
+    alert("Computer wins!");
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
+    return;
+  } else if (checkDraw()) {
+    alert("Draw!");
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
+    return;
+  }
+}
 
-    randomEmptyCell.textContent = "O";
+function findBestMove(board) {
+  let bestValue = -Infinity;
+  let bestMove = null;
 
-    if (gameOver()) {
-      alert("Computer wins!");
-      setTimeout(() => {
-        location.reload();
-      }, 1000);
-      return;
-    } else if (checkDraw()) {
-      alert("Draw!");
-      setTimeout(() => {
-        location.reload();
-      }, 1000);
-      return;
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (board[row][col] === "") {
+        board[row][col] = "O";
+        const moveValue = minimax(board, 0, false);
+        board[row][col] = "";
+        if (moveValue > bestValue) {
+          bestValue = moveValue;
+          bestMove = { row, col };
+        }
+      }
     }
   }
+  return bestMove;
+}
+
+function minimax(board, depth, isMaximizing) {
+  const score = evaluate(board);
+
+  if (score === 10) return score - depth;
+  if (score === -10) return score + depth;
+  if (isBoardFull(board)) return 0;
+
+  if (isMaximizing) {
+    let best = -Infinity;
+    const moves = validMoves(board);
+    moves.forEach((move) => {
+      board[move.row][move.col] = "O";
+      best = Math.max(best, minimax(board, depth + 1, !isMaximizing));
+      board[move.row][move.col] = "";
+    });
+    return best;
+  } else {
+    let best = Infinity;
+    const moves = validMoves(board);
+    moves.forEach((move) => {
+      board[move.row][move.col] = "X";
+      best = Math.min(best, minimax(board, depth + 1, !isMaximizing));
+      board[move.row][move.col] = "";
+    });
+    return best;
+  }
+}
+
+function isBoardFull(board) {
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (board[row][col] === "") return false;
+    }
+  }
+  return true;
+}
+function evaluate(board) {
+  // Check rows
+  for (let row = 0; row < 3; row++) {
+    if (board[row][0] === board[row][1] && board[row][1] === board[row][2]) {
+      if (board[row][0] === "O") return 10;
+      if (board[row][0] === "X") return -10;
+    }
+  }
+
+  // Check columns
+  for (let col = 0; col < 3; col++) {
+    if (board[0][col] === board[1][col] && board[1][col] === board[2][col]) {
+      if (board[0][col] === "O") return 10;
+      if (board[0][col] === "X") return -10;
+    }
+  }
+
+  // Check diagonals
+  if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+    if (board[0][0] === "O") return 10;
+    if (board[0][0] === "X") return -10;
+  }
+
+  if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+    if (board[0][2] === "O") return 10;
+    if (board[0][2] === "X") return -10;
+  }
+
+  return 0;
+}
+
+function validMoves(board) {
+  const moves = [];
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (board[row][col] === "") moves.push({ row, col });
+    }
+  }
+  return moves;
 }
 
 function hasTreeInALine() {
